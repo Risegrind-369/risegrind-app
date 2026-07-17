5dafb676708cd261a333d352390813662a7ad63c
Risegrind-369 <shortsocialclipping@gmail.com>
2026-07-17 12:09:05 +0200

    Change supportsTablet from true to false

--- app.config.fr.ts
+++ app.config.fr.ts
@@ -40,7 +40,7 @@ const config: ExpoConfig = {
   userInterfaceStyle: "automatic",
   newArchEnabled: true,
   ios: {
-    supportsTablet: true,
+    supportsTablet: false,
     bundleIdentifier: env.iosBundleId,
   },
   android: {
